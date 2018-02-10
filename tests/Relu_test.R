092484f0490713df1985a763ecd910b04d206c52
LapsedPacifistROU <zubeyir.oflaz@gmail.com>
2018-02-10 03:19:26 +0100

    testthat troubleshoot

--- tests/Relu_test.R
+++ tests/Relu_test.R
@@ -2,4 +2,10 @@
 
 library(testthat)
 
+activation_Relu <- function(z_val){
+  stopifnot(is.numeric(z_val))
+  return((ifelse(z_val >= 0, z_val, 0)))
+
+}
+
 expect_that(activation_Relu(c(-5,-7,8,-3,0,5,19)),equals(c(0,0,8,0,0,5,19)))
